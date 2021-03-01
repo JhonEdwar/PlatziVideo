a476377b94d5c8995943422a51a441828ab6409e
Jhon Edwar <jhonedwar192@gmail.com>
2021-02-28 20:41:44 -0500

    EXLint

---
+++ src/components/HolaMundo.jsx
@@ -0,0 +1,13 @@
+import React from 'react' 
+import '../assets/style/App.scss'
+
+
+const HolaMundo = (Components)=>{
+return(
+    <div>
+        <h1> hola mundo</h1>
+    </div>
+)
+}
+
+export default HolaMundo    
