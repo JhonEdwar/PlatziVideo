f18741fce52e183d03c09a6260bf8c6ccc3ab9cb
Jhon Edwar <jhonedwar192@gmail.com>
2021-02-28 21:00:57 -0500

    no mentira

--- src/components/HolaMundo.jsx
+++ src/components/HolaMundo.jsx
@@ -5,7 +5,7 @@ import '../assets/style/App.scss'
 const HolaMundo = (Components)=>{
 return(
     <div>
-        <h1> hola mundo care popo</h1>
+        <h1> hola mundo no mentira</h1>
     </div>
 )
 }
